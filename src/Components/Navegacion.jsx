1ed26531aeafadda65c6b8cb2d520d5ac61cb9a2
AlexisBeltran <jesus_beltran011@outlook.com>
2023-03-20 17:55:09 -0600

    Agregó diseño principal de portafolio

---
+++ src/Components/Navegacion.jsx
@@ -0,0 +1,36 @@
+import Github from "../Assets/Img/Github.svg";
+import LinkedIn from "../Assets/Img/LinkedIn.svg";
+import About from "../Assets/Img/About.svg";
+
+const Navegacion = () => {
+  return (
+    <>
+        <nav className="py-7 flex flex-row justify-center md:justify-between items-center">
+            <div className="hidden md:flex gap-x-4">
+                <a className="text-white text-lg" href="">Experiencia</a>
+                <a className="text-white text-lg" href="">Proyectos</a>
+                <a className="text-white text-lg" href="">Contacto</a>
+            </div>
+            <div className="flex justify-center gap-x-4" id="info-web">
+                <img src={Github} alt="Github"/>
+                <img src={LinkedIn} alt="LinkedIn"/>
+            </div>
+        </nav>
+        <header className="py-7 md:relative" id="header-show">
+            <div className="flex md:flex-row items-center md:justify-between">
+                <h1 className="flex flex-col" id="title-main">
+                    ¡Hola!{""}
+                    <span id="title-main-name">Soy Alexis Beltrán</span>
+                    <span id="title-main-job">Desarrollador web.</span> 
+                </h1>
+            </div>
+            <img src={About} className="md:absolute top-0 right-0"/>
+            
+        </header>
+    </>
+    
+    
+  )
+}
+
+export default Navegacion
---
+++ src/Page/Index.jsx
@@ -0,0 +1,11 @@
+import Navegacion from "../Components/Navegacion";
+const Index = () => {
+  return (
+    <main className="md:container md:mx-auto h-screen container mx-auto">
+      <Navegacion/>  
+    </main>
+      
+  )
+}
+
+export default Index
