99bc16cb56172dbf009840117156c304278fe38c
AlexisBeltran <jesus_beltran011@outlook.com>
2023-03-20 19:09:15 -0600

    Deleted basename

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import {createBrowserRouter, RouterProvider, BrowserRouter} from "react-router-dom"
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
 import Index from './Page/Index'
 const router = createBrowserRouter([
   {
@@ -13,8 +13,6 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='/PortafolioWeb'>
-      <RouterProvider router={router}/>
-    </BrowserRouter>
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
