999038d580faf286c98759ee4cd12c5f1c354975
AlexisBeltran <jesus_beltran011@outlook.com>
2023-05-30 17:15:55 -0600

    Update works

--- src/Section/Trabajos.jsx
+++ src/Section/Trabajos.jsx
@@ -5,9 +5,10 @@ import 'swiper/css/scrollbar';
 import {Swiper, SwiperSlide} from "swiper/react";
 import SwiperCore, { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper';
 import AgenciaViaje from "./../Assets/Img/agencia_viajes.png"
+import Clima_App from "./../Assets/Img/Clima_App.png"
 import portfolio from "./../Assets/Img/portfolio.png"
 import apirest from "./../Assets/Img/API_REST.png"
-import ArrowLeft from "./../Assets/Icons/Arrow_left.svg"
+
 import ArrowRight from "./../Assets/Icons/Arrow_right.svg"
 import Button from "../Components/Button"
 
@@ -42,7 +43,7 @@ function Trabajos() {
                         <div className="text-left md:text-right flex flex-col justify-around h-full gap-10 items-start">
                             <h2 className="">Portafolio Web</h2>
                             <p className="text-justify">Mi portafolio web es una plataforma donde presento mis habilidades y experiencia profesionales. La página de inicio destaca mis áreas de especialización y habilidades más importantes, junto con ejemplos específicos de proyectos que he realizado en cada área</p>
-                            <p id="title_work" className="text-left">Tecnologias: <span>React, Tailwind, React router v6, CSS, HTML, JavaScript y despliegue con Github Pages</span></p>
+                            <p id="title_work" className="text-left">Tecnologias: <span>React.js, Tailwind, React router v6, CSS, HTML, JavaScript y despliegue con Github Pages</span></p>
                             <a className="flex flex-row-reverse md:flex-row items-center justify-items-start md:justify-end gap-10" href="https://alexisbeltran.github.io/PortafolioWeb/" target="_blank">
                                 Ver proyecto
                                 <span>
@@ -63,6 +64,18 @@ function Trabajos() {
                             <p className="text-justify">API REST para una aplicación de entrenamiento de CrossFit.</p>
                             <p id="title_work">Tecnologias: <span>Node.js, JavaScript, Express, CI/CD.</span></p>
                             <a className="flex flex-row items-center gap-10" href="https://apirestcrossfit-production.up.railway.app/api/v1/docs/" target="_blank">Ver proyecto <span><img src={ArrowRight} alt="arrow_right"/></span></a>
+                        </div>
+                    </div>
+                </SwiperSlide>
+
+                <SwiperSlide>
+                    <div className='md:grid md:grid-cols-2 md:gap-14 flex flex-col items-center gap-5 pb-5'>
+                        <img src={Clima_App} alt="Clima App"/>
+                        <div className='text-left flex flex-col justify-around h-full gap-10'>
+                            <h2>Clima App</h2>
+                            <p className='text-justify'>Buscador de clima en tiempo real ya sea por ciudad, estado o país.</p>
+                            <p id='title_work'>Tecnologias: <span>HTML, CSS, JavaScript, React.js, API, CI/CD</span></p>
+                            <a className='flex flex-row items-center gap-10' href='https://alexisbeltran.github.io/weatherApp/' target='_black'>Ver proyecto <span><img src={ArrowRight} alt="arrow_right"/></span></a>
                         </div>
                     </div>
                 </SwiperSlide>
